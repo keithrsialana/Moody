7b4dbc3be95366d2698b92faf3d220158a04cbe3
Keith Sialana <ninjabasketball@hotmail.com>
2024-11-08 13:59:59 -0500

    added some theme stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,31 +5,29 @@ import "./styles/main.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Includes Popper.js as well
 import { MoodProvider } from "./context/MoodContext";
 import { UserProvider } from "./context/LoginContext";
-import ThemeContext, { ThemeProvider } from "./context/ThemeContext";
+import ThemeContext from "./context/ThemeContext";
 import { useContext } from "react";
 // import React from 'react';
 
-function App() {
-	const themeContext = useContext(ThemeContext);
-	console.log(themeContext);
-	// const {theme} = themeContext;
+const App: React.FC = () => {
+	// console.log(themeContext);
+	const themeContext: any = useContext(ThemeContext);
+	const { theme } = themeContext;
+
 	return (
-		<ThemeProvider>
-			{/* <div className={theme}> */}
-			<div className="theme-light">
-				<UserProvider>
-					<Header />
-					<MoodProvider>
-						<main>
-							{/* Renders the matched route component */}
-							<Outlet />
-						</main>
-					</MoodProvider>
-					<Footer />
-				</UserProvider>
-			</div>
-		</ThemeProvider>
+		<div className={theme}>
+			<UserProvider>
+				<Header />
+				<MoodProvider>
+					<main>
+						{/* Renders the matched route component */}
+						<Outlet />
+					</main>
+				</MoodProvider>
+				<Footer />
+			</UserProvider>
+		</div>
 	);
-}
+};
 
 export default App;
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -3,35 +3,74 @@ import { Link } from "react-router-dom";
 import UserContext from "../context/LoginContext";
 import { useContext } from "react";
 import User from "../interfaces/User";
+import ThemeContext from "../context/ThemeContext";
 
 const NavBar: React.FC = () => {
 	// Add 'Welcome Username' at the end of the navbar if the user is logged in
 	const context: any = useContext(UserContext);
 	const { loggedInUser, setLoggedInUser } = context;
 
+	const themeContext: any = useContext(ThemeContext);
+	const { theme, setTheme } = themeContext;
+
 	async function logout() {
 		setLoggedInUser({} as User);
 	}
 	return (
 		<nav className="d-flex container-fluid justify-content-end">
-      {loggedInUser.username? (<Link to="/" className="text-decoration-none me-3 d-flex align-items-center">
-				<div className="d-flex bg-primary h-50 align-items-center rounded ps-3 pe-3 text-black">
-					Home
+			{theme == "theme-light" ? (
+				<div className="text-decoration-none me-3 d-flex align-items-center">
+					<p
+						className="d-flex bg-primary h-50 align-items-center rounded ps-3 pe-3 text-black"
+						onClick={() => setTheme("theme-dark")}
+					>
+						Change to Dark Theme
+					</p>
 				</div>
-			</Link>):('')}
+			) : (
+				<div className="text-decoration-none me-3 d-flex align-items-center">
+					<p
+						className="d-flex bg-primary h-50 align-items-center rounded ps-3 pe-3 text-black"
+						onClick={() => setTheme("theme-light")}
+					>
+						Change to Light Theme
+					</p>
+				</div>
+			)}
 			{loggedInUser.username ? (
-				<Link to="/login" className="text-decoration-none me-3 d-flex align-items-center" onClick={logout}				>
+				<Link
+					to="/"
+					className="text-decoration-none me-3 d-flex align-items-center"
+				>
+					<div className="d-flex bg-primary h-50 align-items-center rounded ps-3 pe-3 text-black">
+						Home
+					</div>
+				</Link>
+			) : (
+				""
+			)}
+			{loggedInUser.username ? (
+				<Link
+					to="/login"
+					className="text-decoration-none me-3 d-flex align-items-center"
+					onClick={logout}
+				>
 					<div className="d-flex bg-primary h-50 align-items-center rounded ps-3 pe-3 text-black">
 						Logout
 					</div>
 				</Link>
-			) : ("")}
+			) : (
+				""
+			)}
 			{loggedInUser.username ? (
 				<div className="text-decoration-none me-3 d-flex align-items-center">
 					{`Welcome back, ${loggedInUser.username}!`}
 				</div>
 			) : (
-				<Link to="/login" className="text-decoration-none me-3 d-flex align-items-center">
+				<Link
+					to="/login"
+					className="text-decoration-none me-3 d-flex align-items-center"
+				>
 					<div className="d-flex bg-primary h-50 align-items-center rounded ps-3 pe-3 text-black">
 						Login
 					</div>
