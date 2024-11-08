c4e6f4dfe7407763edf09ec0f692c87b14b6170b
limboden <imbodenlu@gmail.com>
2024-11-08 12:46:10 -0500

    added env folder to gitignore

--- client/src/pages/CurrentPlaylist.tsx
+++ client/src/pages/CurrentPlaylist.tsx
@@ -4,8 +4,9 @@ const CurrentPlaylist: React.FC = () => {
 
 
   return (
-    <div>
+    <div className="container text-center pt-5 vh-100">
       <h3>Current playlist based on your mood:</h3>
+      <div>{/*TODO: implement chat gpt playlist response*/}</div>
     </div>
   )
 }
