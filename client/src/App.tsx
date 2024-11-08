a32e75cd55decd0ce3d6986973d2b25bc1312d51
Keith Sialana <ninjabasketball@hotmail.com>
2024-11-07 23:25:04 -0500

    removed some unused stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./styles/main.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Includes Popper.js as well
 import { MoodProvider } from "./context/MoodContext";
-import UserContext, { UserProvider } from "./context/LoginContext";
-import { useContext } from "react";
+import { UserProvider } from "./context/LoginContext";
 
 // import React from 'react';
 
 function App() {
-	// TODO: Use Contexting to return the user either the login page or Outlet depending if they're logged in or not
-	// TODO: Use contexting to change the website theme based on user preference
-
 	return (
 		<div className="theme-dark">
 			<UserProvider>
