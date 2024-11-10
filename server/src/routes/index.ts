a9c870e3271fc58fa180e391423e466eba5de72e
Keith Sialana <ninjabaskaetball@hotmail.com>
2024-11-09 23:15:58 -0500

    added song and playlist routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import userRoutes from './userRoutes.js';
 import spotifyRoutes from './spotifyRoutes.js';
 import authRoutes from './authRoutes.js';
+import songRoutes from './songRoutes.js';
+import playlistRoutes from './playlistRoutes.js';
 import { authenticateToken } from '../middleware/auth.js';
 
 const router = express.Router();
@@ -9,5 +11,7 @@ const router = express.Router();
 router.use('/api', authenticateToken, userRoutes);
 router.use('/spotify', spotifyRoutes);
 router.use('/auth', authRoutes);
+router.use('/songs', songRoutes);
+router.use('/playlists', playlistRoutes);
 
 export default router;
--- server/src/routes/playlistRoutes.ts
+++ server/src/routes/playlistRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createPlaylist, deleteplaylist } from "../controllers/playlistController";
+import { createPlaylist, deleteplaylist, getAllPlaylists, updatePlaylistName } from "../controllers/playlistController";
 const router = Router();
 
 router.post("/add", createPlaylist);
 router.post("/remove", deleteplaylist);
+router.post("/update", updatePlaylistName);
+router.get("/", getAllPlaylists);
 
 export default router;
