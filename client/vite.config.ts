d5bd7439f08f453080f474e86a94b11e19a1855c
Keith Sialana <ninjabasketball@hotmail.com>
2024-11-11 18:18:01 -0500

    maybe this will work?

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,28 +7,33 @@ export default defineConfig({
 			external: ["bootstrap"],
 		},
 	},
-  preview:{
-    port: 10696
-  },
+	preview: {
+		port: 10696,
+	},
 	server: {
 		port: 10696,
 		open: true,
 		proxy: {
 			"/api": {
-				target: "https://moody-lawj.onrender.com:10000",
+				target: "http://localhost:10000",
 				changeOrigin: true,
 				secure: false,
 			},
 			"/auth": {
-				target: "https://moody-lawj.onrender.com:10000",
+				target: "http://localhost:10000",
 				changeOrigin: true,
 				secure: false,
 			},
 			"/openai": {
-				target: "https://moody-lawj.onrender.com:10000",
+				target: "http://localhost:10000",
 				changeOrigin: true,
 				secure: false,
 			},
+			"/*":{
+				target: "http://localhost:10000",
+				changeOrigin: true,
+				secure: false,
+			}
 		},
 	},
 	css: {
