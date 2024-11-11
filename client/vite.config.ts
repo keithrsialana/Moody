e049c1c0c91121ff3fb60cd5caf7948f12d30c12
Keith Sialana <ninjabasketball@hotmail.com>
2024-11-11 18:33:41 -0500

    changed more vite config to see if render hates it again

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,30 +7,21 @@ export default defineConfig({
 			external: ["bootstrap"],
 		},
 	},
-	preview: {
-		port: 10696,
-	},
 	server: {
-		port: 10696,
 		open: true,
 		proxy: {
 			"/api": {
-				target: "http://localhost:10000",
+				target: "https://moody-lawj.onrender.com/api",
 				changeOrigin: true,
 				secure: false,
 			},
 			"/auth": {
-				target: "http://localhost:10000",
+				target: "https://moody-lawj.onrender.com/auth",
 				changeOrigin: true,
 				secure: false,
 			},
 			"/openai": {
-				target: "http://localhost:10000",
-				changeOrigin: true,
-				secure: false,
-			},
-			"/*":{
-				target: "http://localhost:10000",
+				target: "https://moody-lawj.onrender.com/openai",
 				changeOrigin: true,
 				secure: false,
 			}
