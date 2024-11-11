7f8c41555d489f5165e741057980fa82f374d9a0
Keith Sialana <ninjabasketball@hotmail.com>
2024-11-11 18:40:19 -0500

    daft punk "one more time"

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,30 @@
-import express from 'express';
-import sequelize from './database.js';
-import allRoutes from './routes/index.js';
-import { User } from './models/index.js';
-import users from './dbusers.js';
+import express from "express";
+import sequelize from "./database.js";
+import allRoutes from "./routes/index.js";
+import { User } from "./models/index.js";
+import users from "./dbusers.js";
+import path from "path";
+
+
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Middleware for parsing JSON
-app.use(express.static('../../client/dist'));
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 app.use(express.json());
 
 // Use the user routes
 app.use(allRoutes);
 
-sequelize.sync({force:true}).then(async () => {
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
-    await User.bulkCreate(users)
-    .then(() => console.log('Users inserted'))
-    .catch(err => console.error('Error inserting users:', err));
+app.get("*", (_req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
+sequelize.sync({ force: true }).then(async () => {
+	app.listen(port, () => {
+		console.log(`Server is running on http://localhost:${port}`);
+	});
+	await User.bulkCreate(users)
+		.then(() => console.log("Users inserted"))
+		.catch((err) => console.error("Error inserting users:", err));
+});
