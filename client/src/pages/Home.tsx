f5f5ed15bcfef69fcad1ba6699181a1fbef5203b
Keith <34338831+keithrsialana@users.noreply.github.com>
2024-11-15 02:01:56 -0500

    updated the playlist to user-based, and openai songs more randomized

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,16 +4,7 @@ import { useNavigate } from "react-router-dom";
 import UserContext from "../context/LoginContext";
 import axios from 'axios';
 
-function sendMood(mood: string) {
-  axios.post('/openai/api/data', { mood })
-    .then(response => {
-      console.log('Response from server:', response.data);
-      localStorage.setItem("playlist", JSON.stringify(response.data));
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-}
+
 
 const Home: React.FC = () => {
 
@@ -21,31 +12,39 @@ const Home: React.FC = () => {
   const context = useContext(MoodContext);
   const userContext: any = useContext(UserContext);
   const { loginToken } = userContext;
-  const naviate = useNavigate();
+  const navigate = useNavigate();
 
   // if user isn't logged in, forcefully push user to login page
   useEffect(() => {
     if (!loginToken.username)
-      naviate('/login');
+      navigate('/login');
   }, []);
 
   if (!context) {
     throw new Error("Mood must be used within a MoodProvider");
   }
-  const { mood, setMood } = context;
-  React.useEffect(() => {
-    console.log(`the mood is now ${mood}`);
-    sendMood(mood)
-  }, [mood]);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  async function sendMood(mood: string) {
+    await axios.post('/openai/api/data', { mood })
+      .then(response => {
+        console.log('Response from server:', response.data);
+        const playlistObj = {username: loginToken.username, playlist: response.data};
+        localStorage.setItem(`${loginToken.username}_playlist`, JSON.stringify(playlistObj));
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+
+  const { mood, setMood } = context;
+  
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(`the mood was ${mood}`);
-    console.log('Submitted value:', inputValue);
     // store mood in database, also store current datetime. this will be for the history later. 
     // pass mood to the playlist page using useContext
     setMood(inputValue);
-    naviate('/currentplaylist');
+    await sendMood(mood);
+    navigate('/currentplaylist');
 
   };
 
